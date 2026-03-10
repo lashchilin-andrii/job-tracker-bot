eed2c7ed97290614d7fa8b01fd37ad2f2d5b6e2c
Andrew <lashchilin.andrii.@gmail.com>
2026-03-11 00:42:37 +0300

    feat: add logger middleware

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 
+from src.logging import LoggingMiddleware
 from src.config import BotConfig
 from src.job.handler import router as job_router
 from src.user.handler import router as user_router
@@ -10,10 +11,14 @@ from src.base.handler import router as base_router
 
 dp = Dispatcher()
 
+dp.include_router(user_router)
 dp.include_router(job_router)
 dp.include_router(user_job_router)
-dp.include_router(user_router)
-dp.include_router(base_router)
+dp.include_router(base_router)  # Must be last cuz contains fallback handler
+
+
+dp.message.middleware(LoggingMiddleware())
+dp.callback_query.middleware(LoggingMiddleware())
 
 
 async def main() -> None:
